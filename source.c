7b69b0b973040abc724e8e3b0ab617fb67cf0a25
Seongrae Kim <gimsung2007@icloud.com>
2023-11-03 10:02:53 +0900

    바보짓

--- source.c
+++ source.c
@@ -8,8 +8,8 @@ void jasan(){
 int seed=100000000;
 int main(){
     int select;
-    printf("/n/n/n/n/n/n/n/n/n/n");
-    printf("작업 선택\n 시장 보기 : 1  자산 보기 : 2");
+    printf("\n \n \n \n \n \n \n \n \n \n");
+    printf("작업 선택\n시장 보기 : 1  자산 보기 : 2");
     scanf("%d",&select);
     if (select == 1){
         sijang();
