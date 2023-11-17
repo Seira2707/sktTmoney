aceda1c61cc14fc20ab665c3fb4ab2738b336966
StarLas <88621958+dragun1016@users.noreply.github.com>
2023-11-17 10:32:41 +0900

    open ssl + curl

---
+++ UpbitReq.c
@@ -0,0 +1,91 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+
+
+struct url_data {
+    size_t size;
+    char* data;
+};
+
+size_t write_data(void* ptr, size_t size, size_t nmemb, struct url_data* data) {
+    size_t index = data->size;
+    size_t n = (size * nmemb);
+    char* tmp;
+
+    data->size += (size * nmemb);
+
+#ifdef DEBUG
+    fprintf(stderr, "data at %p size=%ld nmemb=%ld\n", ptr, size, nmemb);
+#endif
+
+    tmp = realloc(data->data, data->size + 1); /* +1 for '\0' */
+
+    if (tmp) {
+        data->data = tmp;
+    }
+    else {
+        if (data->data) {
+            free(data->data);
+        }
+        fprintf(stderr, "Failed to allocate memory.\n");
+        return 0;
+    }
+
+    memcpy((data->data + index), ptr, n);
+    data->data[data->size] = '\0';
+
+    return size * nmemb;
+}
+
+char* handle_url(char* url) {
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    CURL* curl;
+
+    struct url_data data;
+    data.size = 0;
+    data.data = malloc(4096); /* reasonable size initial buffer */
+    if (NULL == data.data) {
+        fprintf(stderr, "Failed to allocate memory.\n");
+        return NULL;
+    }
+
+    data.data[0] = '\0';
+
+    CURLcode res;
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+        }
+
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+
+    return data.data;
+}
+
+int main() {
+    char* url_get = "https://api.upbit.com/v1/ticker?markets=KRW-BTC";
+    char* data;
+
+    data = handle_url(url_get);
+
+    if (data) {
+        printf("%s\n", data);
+        free(data);
+    }
+
+    return 0;
+}
